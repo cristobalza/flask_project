715b06284aecd24c5eae3acf5f2ecdf89b30737e
cristobalza <cristobalzamas@gmail.com>
2022-06-13 08:52:09 -0400

    added db deletion

--- forms.py
+++ forms.py
@@ -5,18 +5,18 @@ import email_validator
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[InputRequired(), Email()])
-
     password = PasswordField('Password', validators=[InputRequired()])
-
     submit = SubmitField('Login')
 
 class SignUpForm(FlaskForm):
     full_name = StringField('Full Name', validators = [InputRequired()])
-
     email = StringField('Email', validators = [InputRequired(), Email()])
-    
     password = PasswordField('Password', validators = [InputRequired()])
-
     confirm_password = PasswordField('Confirm Password', validators = [InputRequired(), EqualTo('password')])
-
     submit = SubmitField('Sign Up')
+
+class EditPetForm(FlaskForm):
+    name = StringField("Pet's Name", validators = [InputRequired()])
+    age = StringField("Pet's Age", validators = [InputRequired()])
+    bio = StringField("Pet's Bio", validators = [InputRequired()])
+    submit = SubmitField("Edit Pet")
